7526a22da16a2bee52acd0cd00ff84fd5d7c2143
Matis Alias-Bagarre <matis.alias-bagarre@inserm.fr>
2024-09-02 18:47:44 +0200

    test decompose

--- src/decompose_chunk_graph.py
+++ src/decompose_chunk_graph.py
@@ -6,7 +6,7 @@ from bdsg.bdsg import PackedGraph
 
 class Graph_chunk :
     def __init__(self, path_pangenome, path_distance) -> None:
-        self.list_idx_decompose = []
+        self.list_chunk= []
         self.pg = self._initilise_pangenome(path_pangenome)
         self.stree = self._initilise_distance(path_distance)
         self.root = self.stree.get_root()
@@ -21,18 +21,17 @@ class Graph_chunk :
         stree.deserialize(path_distance)
         return stree
     
-    def save_snarl_tree_node(self, net, snarls) -> bool :
-        if self.stree.is_snarl(net):
-            snarls.append(net)
-        if not self.stree.is_node(net) and self.stree.is_sentinel(net):
-            self.stree.for_each_child(net, self.save_snarl_tree_node)
-        return (True)
-    
     def loop_over_children(self) -> list :
-        snarls = []
-        self.stree.for_each_child(self.root, self.save_snarl_tree_node)
-        snarls_length = len(snarls)
-        print('{} snarls found'.format(snarls_length))
+        
+        def save_snarl_tree_node(net) -> bool :
+            chains.append(net)
+            return (True)
+
+        chains = []
+        self.stree.for_each_child(self.root, save_snarl_tree_node)
+        
+        chains_length = len(chains)
+        print('{} chains found'.format(chains_length))
 
         return snarls
 
@@ -50,7 +49,7 @@ class Graph_chunk :
 
             if curr_dist > distance_max :
                 curr_dist = 0
-                self.list_idx_decompose.append(save_snarl)
+                self.list_chunk.append(save_snarl)
             
             save_snarl = send
 
@@ -59,12 +58,9 @@ class Graph_chunk :
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        for index, sublist in enumerate(self.list_idx_decompose):
-            file_path = os.path.join(directory, f"sublist_{index + 1}.txt")
-            
-            with open(file_path, 'w') as file:
-                for item in sublist:
-                    file.write(f"{item}\n")
+        with open(directory, 'w') as file:
+            for line in self.list_chunk :
+                file.write(f"{line}\n")
 
 if __name__ == "__main__" :
     parser = argparse.ArgumentParser(description="Parse and analyse snarl from vcf file")
@@ -73,14 +69,14 @@ if __name__ == "__main__" :
     parser.add_argument('-o', "--output", help='the output dir where all .dist and .pg will be decompose', required=False)
 
     args = parser.parse_args()
-    graph = Graph_chunk()
+    graph = Graph_chunk(args.pangenome, args.distance)
 
     snarls = graph.loop_over_children()
-    graph.indentify_chunk()
+    #graph.indentify_chunk(snarls)
 
-    if args.o :
-        graph.decompose(args.o)
-    else :
-        graph.decompose()
+    # if args.o :
+    #     graph.decompose(args.o)
+    # else :
+    #     graph.decompose()
 
 
