df23206bb29f7b2a6da1ab93a071d64c75350026
Matis Alias-Bagarre <matis.alias-bagarre@inserm.fr>
2024-09-02 18:08:12 +0200

    clean decompose

--- src/decompose_chunk_graph.py
+++ src/decompose_chunk_graph.py
@@ -5,67 +5,82 @@ import bdsg
 from bdsg.bdsg import PackedGraph
 
 class Graph_chunk :
-    def __init__(self, pg_file_path,  dist_file_path) -> None:
+    def __init__(self, path_pangenome, path_distance) -> None:
+        self.list_idx_decompose = []
+        self.pg = self._initilise_pangenome(path_pangenome)
+        self.stree = self._initilise_distance(path_distance)
+        self.root = self.stree.get_root()
 
-        self.pg = PackedGraph()
-        self.pg.deserialize(pg_file_path)
+    def _initilise_pangenome(self, path_pangenome) :
+        pg = bdsg.bdsg.PackedGraph()
+        pg.deserialize(path_pangenome)
+        return pg
 
-        self.dist = PackedGraph()
-        self.dist.deserialize(dist_file_path) 
+    def _initilise_distance(self, path_distance) :
+        stree = bdsg.bdsg.SnarlDistanceIndex()
+        stree.deserialize(path_distance)
+        return stree
+    
+    def save_snarl_tree_node(self, net, snarls) -> bool :
+        if self.stree.is_snarl(net):
+            snarls.append(net)
+        if not self.stree.is_node(net) and self.stree.is_sentinel(net):
+            self.stree.for_each_child(net, self.save_snarl_tree_node)
+        return (True)
+    
+    def loop_over_children(self) -> list :
+        snarls = []
+        self.stree.for_each_child(self.root, self.save_snarl_tree_node)
+        snarls_length = len(snarls)
+        print('{} snarls found'.format(snarls_length))
 
-    def decompose(self) :
-        ...
+        return snarls
 
+    def indentify_chunk(self, snarls, distance_max=100000) -> list :
+        curr_dist = 0
+        # for each snarl, lists paths through the netgraph
+        for _, snarl in enumerate(snarls):
+
+            # create a snarl ID as LEFT_RIGTH bondary nodes
+            sstart = self.stree.get_bound(snarl, False, True)
+            sstart = self.stree.get_node_from_sentinel(sstart)
+            send = self.stree.get_bound(snarl, True, True)
+            send = self.stree.get_node_from_sentinel(send)
+            curr_dist += send - sstart
+
+            if curr_dist > distance_max :
+                curr_dist = 0
+                self.list_idx_decompose.append(save_snarl)
+            
+            save_snarl = send
+
+    def decompose(self, directory='output_pangenome_decomposed') :
+        # Ensure the directory exists
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        for index, sublist in enumerate(self.list_idx_decompose):
+            file_path = os.path.join(directory, f"sublist_{index + 1}.txt")
+            
+            with open(file_path, 'w') as file:
+                for item in sublist:
+                    file.write(f"{item}\n")
 
 if __name__ == "__main__" :
     parser = argparse.ArgumentParser(description="Parse and analyse snarl from vcf file")
     parser.add_argument('-p', "--pangenome", help='the input pangenome .pg file', required=True)
-    parser.add_argument('-d', "--distance", help='the input distance index .dist file',required=True)
+    parser.add_argument('-d', "--distance", help='the input distance index .dist file', required=True)
+    parser.add_argument('-o', "--output", help='the output dir where all .dist and .pg will be decompose', required=False)
+
     args = parser.parse_args()
-    #graph = Graph_chunk(args.pangenome)
+    graph = Graph_chunk()
 
-    pg = bdsg.bdsg.PackedGraph()
-    pg.deserialize(args.pangenome)
+    snarls = graph.loop_over_children()
+    graph.indentify_chunk()
 
-    stree = bdsg.bdsg.SnarlDistanceIndex()
-    stree.deserialize(args.distance)
-    root = stree.get_root()
+    if args.o :
+        graph.decompose(args.o)
+    else :
+        graph.decompose()
 
-    snarls = []
-    sentinels = []
-    nodes = []
-    itr = 0
-    def save_snarl_tree_node(net):
-        if len(snarls) >= 20 :
-            return
-        if stree.is_sentinel(net):
-            sentinels.append(net)
-
-        if stree.is_snarl(net):
-            snarls.append(net)
-
-        if stree.is_node(net):
-            nodes.append(net)
-
-        if not stree.is_node(net) and not stree.is_sentinel(net):
-            stree.for_each_child(net, save_snarl_tree_node)
-        return (True)
-
-    stree.for_each_child(root, save_snarl_tree_node)
-    snarls_length = len(snarls)
-    print("snarl : ", snarls)
-    print("sentinels : ", sentinels)
-    print("nodes : ", nodes)
-
-    # for each snarl, lists paths through the netgraph and write to output TSV
-    for idx, snarl in enumerate(snarls):
-
-        # create a snarl ID as LEFT_RIGTH bondary nodes
-        sstart = stree.get_bound(snarl, False, True)
-        sstart = stree.get_node_from_sentinel(sstart)
-        send = stree.get_bound(snarl, True, True)
-        send = stree.get_node_from_sentinel(send)
-        snarl_id = '{}_{}'.format(stree.node_id(sstart),
-                                stree.node_id(send))
-        print("snarl_id : ", snarl_id)
 
