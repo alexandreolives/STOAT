43fecbdcb65a93cf7c7965eb52f5e98749713971
Plogeur <matis84700@gmail.com>
2024-12-16 11:14:23 +0100

    last try before leave

--- tests/test_binary.py
+++ tests/test_binary.py
@@ -3,33 +3,15 @@ import os
 import pytest
 from unittest.mock import patch
 
-# Add the ../src directory to sys.path to import snarl_analyser
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-
-import stoat
-import list_snarl_paths
-import snarl_analyser
-import utils
-import p_value_analysis
-import write_position
-import gaf_creator
-import time
-import logging
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 @pytest.fixture
 def mock_input_files(tmpdir):
-    # Create temporary files to simulate the input files
     pg_file = tmpdir.join("test/simulation/pg.full.pg")
     dist_file = tmpdir.join("test/simulation/pg.dist")
     vcf_file = tmpdir.join("test/simulation/merged_output.vcf")
     phenotype_file = tmpdir.join("test/simulation/phenotype.tsv")
     output_dir = tmpdir.mkdir("test/simulation/simulation_output")
-
-    # Write sample content to these files
-    pg_file.write("sample data")
-    dist_file.write("sample data")
-    vcf_file.write("sample data")
-    phenotype_file.write("sample data")
     
     return {
         'pg_file': pg_file,
@@ -40,9 +22,8 @@ def mock_input_files(tmpdir):
     }
 
 def test_snarl_analyser(mock_input_files):
-    # Use mock input files in your test
     args = [
-        'src/stoat.py',  # This is the main script you want to test
+        'src/stoat.py',
         '-p', str(mock_input_files['pg_file']),
         '-d', str(mock_input_files['dist_file']),
         '-v', str(mock_input_files['vcf_file']),
@@ -52,12 +33,10 @@ def test_snarl_analyser(mock_input_files):
 
     # Mocking sys.argv to simulate the command-line arguments
     with patch.object(sys, 'argv', args):
-        from stoat import main  # Import the entry point of your script
+        from src.stoat import main
         main()
 
     # Verify that the output directory exists
     assert mock_input_files['output_dir'].exists()
 
     # python3 src/stoat.py -p test/simulation/pg.full.pg -d test/simulation/pg.dist -v test/simulation/merged_output.vcf.gz -b test/simulation/phenotype.tsv -o test/simulation
-
-    assert mock_input_files['output_dir'].exists()
