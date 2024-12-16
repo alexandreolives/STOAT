7cf0506705f52f5e67abef4aa6d5cc545ca71ba0
Matis Alias-Bagarre <matis.alias-bagarre@inserm.fr>
2024-12-16 14:13:19 +0100

    modify test

--- tests/test_binary.py
+++ tests/test_binary.py
@@ -2,41 +2,35 @@ import sys
 import os
 import pytest
 from unittest.mock import patch
+from pathlib import Path
 
+# Add the parent directory to the Python path to enable imports from src
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 @pytest.fixture
-def mock_input_files(tmpdir):
-    pg_file = tmpdir.join("test/simulation/pg.full.pg")
-    dist_file = tmpdir.join("test/simulation/pg.dist")
-    vcf_file = tmpdir.join("test/simulation/merged_output.vcf")
-    phenotype_file = tmpdir.join("test/simulation/phenotype.tsv")
-    output_dir = tmpdir.join("test/simulation/simulation_output")
-    
-    return {
-        'pg_file': pg_file,
-        'dist_file': dist_file,
-        'vcf_file': vcf_file,
-        'phenotype_file': phenotype_file,
-        'output_dir': output_dir
-    }
+def test_snarl_analyser():
+    pg_file = "test/simulation/pg.full.pg"
+    dist_file = "test/simulation/pg.dist"
+    vcf_file = "test/simulation/merged_output.vcf"
+    phenotype_file = "test/simulation/phenotype.tsv"
+    output_dir = Path("test/simulation/simulation_output")  # Use Path for easier path handling
 
-def test_snarl_analyser(mock_input_files):
+    # Command-line arguments to simulate
     args = [
         'src/stoat.py',
-        '-p', str(mock_input_files['pg_file']),
-        '-d', str(mock_input_files['dist_file']),
-        '-v', str(mock_input_files['vcf_file']),
-        '-b', str(mock_input_files['phenotype_file']),
-        '-o', str(mock_input_files['output_dir'])
+        '-p', pg_file,
+        '-d', dist_file,
+        '-v', vcf_file,
+        '-b', phenotype_file,
+        '-o', str(output_dir)
     ]
 
-    # Mocking sys.argv to simulate the command-line arguments
+    # Mock sys.argv to simulate the command-line arguments
     with patch.object(sys, 'argv', args):
         from src.stoat import main
         main()
 
-    # Verify that the output directory exists
-    assert mock_input_files['output_dir'].exists()
+    # Verify that the output directory exists after execution
+    assert output_dir.exists(), f"Output directory {output_dir} does not exist."
 
     # python3 src/stoat.py -p test/simulation/pg.full.pg -d test/simulation/pg.dist -v test/simulation/merged_output.vcf.gz -b test/simulation/phenotype.tsv -o test/simulation
